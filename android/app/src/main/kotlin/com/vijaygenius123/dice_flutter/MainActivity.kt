8037cee3db70c8a8b7f7278789121b52a117e277
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-02-06 23:23:57 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/vijaygenius123/dice_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vijaygenius123.dice_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    )
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container()
+  }
+}
